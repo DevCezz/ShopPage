81af6fd3f0fe0ae56e4f29d7ded41b43a0878c6b
Cezary Sanecki <csanecki@gmail.com>
2019-03-18 10:48:42 +0100

    initial commit

---
+++ src/main/java/pl/csanecki/AITSI/AitsiApplication.java
@@ -0,0 +1,13 @@
+package pl.csanecki.AITSI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AitsiApplication.class, args);
+	}
+
+}
