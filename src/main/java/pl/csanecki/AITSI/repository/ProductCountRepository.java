7369c02d0bc562bb984cc374229f0853b04e4927
Cezary Sanecki <csanecki@gmail.com>
2019-03-22 12:31:25 +0100

    added repository for ProductCount

---
+++ src/main/java/pl/csanecki/AITSI/repository/ProductCountRepository.java
@@ -0,0 +1,9 @@
+package pl.csanecki.AITSI.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pl.csanecki.AITSI.entity.ProductCount;
+
+public interface ProductCountRepository extends JpaRepository<ProductCount, Long> {
+	ProductCount getProductCountByProductId(long productId);
+}
