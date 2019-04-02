a191c4cc8e98b82a35c8c42b774d0c3bf54cc156
Cezary Sanecki <csanecki@gmail.com>
2019-04-02 12:07:15 +0200

    cart is added, needed: - add functionality to change amount of product - clear cart - show info that cart is empty

--- src/main/java/pl/csanecki/AITSI/controller/OrderController.java
+++ src/main/java/pl/csanecki/AITSI/controller/OrderController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,6 +40,6 @@ public class OrderController {
 
         model.addAttribute("cart", cart);
 
-        return "cart";
+        return "redirect:/order/cart";
     }
 }
