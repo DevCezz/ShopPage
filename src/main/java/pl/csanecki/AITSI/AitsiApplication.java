aa213cbf9a99b8d0ea41603f129b4187cb34fa43
csanecki <csanecki@gmail.com>
2019-04-10 00:43:16 +0200

    needed to start on server

--- src/main/java/pl/csanecki/AITSI/AitsiApplication.java
+++ src/main/java/pl/csanecki/AITSI/AitsiApplication.java
@@ -2,9 +2,10 @@ package pl.csanecki.AITSI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AitsiApplication {
+public class AitsiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AitsiApplication.class, args);
